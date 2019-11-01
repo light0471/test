6410ff2c3af463a22bf977e7a752d493a2eed9a7
tianshu <shutian@vip.sina.com>
2019-11-01 14:35:29 +0800

    增加夜间模式

--- pages/setting/about/about.js
+++ pages/setting/about/about.js
@@ -1,14 +1,24 @@
 const app = getApp();
 Page({
-  data: {
-    StatusBar: app.globalData.StatusBar,
-    CustomBar: app.globalData.CustomBar,
-    ColorList: app.globalData.ColorList,
-  },
-  onLoad: function () { },
-  pageBack() {
-    wx.navigateBack({
-      delta: 1
-    });
-  }
+    data: {
+        StatusBar: app.globalData.StatusBar,
+        CustomBar: app.globalData.CustomBar,
+        ColorList: app.globalData.ColorList,
+    },
+    onLoad: function () {
+        var that = this;
+        wx.getStorage({
+            key: 'skins',
+            success: function (res) {
+                that.setData({
+                    SkinStyle: res.data
+                })
+            },
+        })
+    },
+    pageBack() {
+        wx.navigateBack({
+            delta: 1
+        });
+    }
 });
--- pages/setting/setting/setting.js
+++ pages/setting/setting/setting.js
@@ -1,57 +1,45 @@
-const app = getApp();
-// Page({
-// //   data: {
-// //     StatusBar: app.globalData.StatusBar,
-// //     CustomBar: app.globalData.CustomBar,
-// //     ColorList: app.globalData.ColorList,
-// //   },
-// //   onLoad: function () { },
-// //   pageBack() {
-// //     wx.navigateBack({
-// //       delta: 1
-// //     });
-// //   }
-// // });
+var app = getApp();
 Page({
 
-    data:{
+    data: {
+        StatusBar: app.globalData.StatusBar,
+        CustomBar: app.globalData.CustomBar,
+        ColorList: app.globalData.ColorList,
         ScreenBrightness: '',
-
-        fontSize:20
-
+        fontSize: 20,
+        SkinStyle: "normal"
     },
+    onLoad: function () {
+        var that = this;
 
-    // magnifyFontSize(){
-    //
-    //     this.setData({
-    //
-    //         fontSize:this.data.fontSize+3
-    //
-    //     })
-    //
-    // },
-    //
-    // shrinkFontSize(){
-    //
-    //     this.setData({
-    //
-    //         fontSize:this.data.fontSize-3
-    //
-    //     })
-    //
-    // },
-    changeFontSize:function(e){
-        // var that= this;
+        wx.getStorage({
 
-        this.setData({
+            key: 'skins',
 
-            fontSize:parseInt(e.detail.value).toFixed()
+            success: function (res) {
+
+                that.setData({
+
+                    SkinStyle: res.data
+
+                })
+
+            },
 
         })
-    // wx.CanvasContext.font({value:parseInt(e.detail.value).toFixed(1)})
-},
-    //监听页面加载
-    changeScreenLight:function(e){
+    },
+    pageBack() {
+        wx.navigateBack({
+            delta: 1
+        })
+    },
+    changeFontSize: function (e) {
+        this.setData({
+            fontSize: parseInt(e.detail.value).toFixed()
+        })
+    },
+
+    changeScreenLight: function (e) {
         var that = this;
         //滑动拉杆获得值
         wx.setScreenBrightness({
@@ -59,13 +47,52 @@ Page({
         })
         //给屏幕亮度赋值
         wx.getScreenBrightness({
-            success: function(res) {
+            success: function (res) {
                 that.setData({
                     ScreenBrightness: res.value
                 })
             }
         })
+    },
+    bgBtn: function () {
+
+        if (this.data.SkinStyle === "normal") {
+
+            app.globalData.skin = "dark"; //设置app（）中皮肤的类型
+
+            this.setData({
+
+                SkinStyle: app.globalData.skin //设置SkinStyle的值
+
+            })
+
+            wx.setStorage({   //设置storage
+
+                key: 'skins',
+
+                data: app.globalData.skin,
+
+            })
+
+        } else {
+
+            app.globalData.skin = "normal";
+
+            this.setData({
+
+                SkinStyle: "normal"
+
+            })
+
+            wx.setStorage({
+
+                key: 'skins',
+
+                data: app.globalData.skin,
+
+            })
+
+        }
+
     }
-
-
 })
