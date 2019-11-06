035e06241497c4dcccb6561edbb6e1ef1ba15816
Yirmeyahu <3246106081@qq.com>
2019-11-06 09:55:59 +0800

    修改图片，增加路径

---
+++ pages/study/home/home.js
@@ -0,0 +1,13 @@
+Component({
+  options: {
+    addGlobalClass: true,
+  },
+  data: {
+    elements: [
+      { title: '课表', name: 'kb', color: 'purple', icon: 'vipcard' },
+      { title: '教案', name: 'ja', color: 'mauve', icon: 'formfill' },
+      { title: '作业', name: 'homework', color: 'pink', icon: 'list' },
+      { title: '签到', name: 'signin', color: 'brown', icon: 'newsfill' }
+    ],
+  },
+})
